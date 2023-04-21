ee8b4eaabda6ebf8d82d29d8be40bed2fa5ca9e1
huaian_zhou <huaian_zhou@outlook.com>
2023-04-21 22:07:36 +0800

    实现常用激活函数算子

---
+++ src/layer/details/sigmoid.hpp
@@ -0,0 +1,19 @@
+#ifndef TINY_INFER_SOURCE_LAYER_SIGMOID_HPP_
+#define TINY_INFER_SOURCE_LAYER_SIGMOID_HPP_
+
+#include "layer/abstract/no_attr_layer.hpp"
+
+namespace TinyInfer {
+
+class Sigmoid : public NoAttrLayer {
+public:
+    explicit Sigmoid();
+
+    InferStatus Forward(const std::vector<sftensor>& inputs, std::vector<sftensor>& outputs) override;
+
+    static ParseParamAttrStatus GetInstance(const srunop& op, slayer& sigmoid);
+};
+
+}
+
+#endif  // TINY_INFER_SOURCE_LAYER_SIGMOID_HPP_
